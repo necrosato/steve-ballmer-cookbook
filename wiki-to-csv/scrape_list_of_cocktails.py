3ece906bf0529ff33274a31214a77259800767a5
Naookie Sato <naookie@revl.com>
2019-01-31 16:46:51 -0800

    removed dups

--- wiki-to-csv/scrape_list_of_cocktails.py
+++ wiki-to-csv/scrape_list_of_cocktails.py
@@ -13,7 +13,6 @@ from bs4 import BeautifulSoup as bs
 def main():
     all_to_url = {}
     all_to_html_filenames = {}
-    index_to_title = {}
     
     loc_request = requests.get("https://en.wikipedia.org/wiki/List_of_cocktails")
     loc_soup = bs(loc_request.text, 'html.parser')
@@ -30,19 +29,17 @@ def main():
         html_filename = "./data/html/{:04d}.html".format(i)
         html = link_soup.prettify()
 
-        all_to_url[title] = url
-        all_to_html_filenames[title] = html_filename
-        index_to_title[i] = title
-        with open(html_filename, "w") as html_file:
-            html_file.write(html)
-        i+=1
+        if title not in all_to_url:
+            all_to_url[title] = url
+            all_to_html_filenames[title] = html_filename
+            with open(html_filename, "w") as html_file:
+                html_file.write(html)
+            i+=1
 
     with open("./data/all_links.yml", "w") as all_links:
         yaml.dump(all_to_url, all_links, default_flow_style=False)
     with open("./data/all_links_html_filenames.yml", "w") as all_links_html_filenames:
         yaml.dump(all_to_html_filenames, all_links_html_filenames, default_flow_style=False)
-    with open("./data/index_to_title.yml", "w") as index_to_title_yml:
-        yaml.dump(index_to_title, index_to_title_yml, default_flow_style=False)
 
 if __name__ == "__main__":
     main()
